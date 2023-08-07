f207a4a73717e33d56f1116306ffc0ecf29a68c6
Salal Aslam <salalaslam@gmail.com>
2023-08-07 11:32:33 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,43 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <ThemeRegistry>
+          <AppBar
+            position="fixed"
+            sx={{
+              bgcolor: lightBlue[100],
+              border: "3px solid #000",
+              color: "#000",
+              boxShadow: "8px 8px 0 0 #000",
+              mr: 0.5,
+              borderRadius: 3,
+            }}
+            elevation={0}
+          >
+            <Toolbar>
+              <Typography variant="h6" noWrap component="div">
+                {0} pending tasks
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Box
+            component="main"
+            sx={{
+              bgcolor: "background.default",
+              height: "100vh",
+            }}
+          >
+            <Box
+              sx={{
+                mt: {
+                  xs: 8,
+                  sm: 9,
+                },
+                p: 3,
+              }}
+            >
               {children}
+            </Box>
+          </Box>
         </ThemeRegistry>
       </body>
     </html>
